6615bfc39534dc062a092020a647edfe76377c19
LucasBBacon <lucasbrumatti@hotmail.com>
2026-02-10 22:35:27 +0000

    Added further context to items.

--- form_manager/src/models/item.py
+++ form_manager/src/models/item.py
@@ -17,6 +17,13 @@ class DamageType(str, Enum):
     PSYCHIC = "psychic"
     RADIANT = "radiant"
     THUNDER = "thunder"
+    
+
+@dataclass
+class ArmorClass:
+    base: int = 0
+    dex_bonus: bool = False
+    max_dex_bonus: int = 0
 
 
 @dataclass
@@ -24,6 +31,10 @@ class Item:
     name: str
     stackable: bool = False
     quantity: int = 1
+    
+    cost: Dict[str, int] = field(default_factory=lambda: {
+        "cp": 0, "sp": 0, "ep": 0, "gp": 0, "pp": 0
+    })
     
     category: Optional[str] = None
     equipped: bool = False
@@ -34,6 +45,7 @@ class Item:
     damage_dice: Optional[str] = None
     damage_type: Optional[DamageType] = None
     range: Optional[str] = None
+    armor_class: Optional[ArmorClass] = None
     
     def __post_init__(self):
         pass
@@ -84,3 +96,29 @@ class Item:
             
         if 'category' in template_data:
             self.category = template_data['category']
+            
+        if 'cost' in template_data:
+            self.cost = template_data['cost']
+            
+        if 'armor_class' in template_data:
+            armor_class_data = template_data['armor_class']
+            base = 0
+            dex_bonus = False
+            max_dex_bonus = 0
+            if 'base' in armor_class_data:
+                base = armor_class_data['base']
+            if 'dex_bonus' in armor_class_data:
+                dex_bonus = bool(armor_class_data['dex_bonus'])
+            if 'max_dex_bonus' in armor_class_data:
+                max_dex_bonus = armor_class_data['max_dex_bonus']
+            armor_class = ArmorClass(base=base,
+                                     dex_bonus=dex_bonus,
+                                     max_dex_bonus=max_dex_bonus)
+            self.armor_class = armor_class
+            
+        if 'strength_requirement' in template_data:
+            self.strength_requirement = template_data['strength_requirement']
+        
+        if 'stealth_disadvantage' in template_data:
+            self.stealth_disadvantage = template_data['stealth_disadvantage']
+        
